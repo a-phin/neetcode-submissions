67bcd0ad40a1119e9283bdbfe53d12987cd2ebac
Angelina Phin <vkpfxfhz48@privaterelay.appleid.com>
2026-08-21 00:16:44 -0400

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.java
@@ -0,0 +1,32 @@
+class MinStack {
+
+    private Stack<Integer> stackOne;
+    private Stack<Integer> stackTwo;
+
+    public MinStack() {
+        stackOne = new Stack<Integer>();
+        stackTwo = new Stack<Integer>();
+    }
+    
+    public void push(int val) {
+        stackOne.push(val);
+        if (stackTwo.empty() || val < stackTwo.peek()) {
+            stackTwo.push(val);
+        }
+    }
+    
+    public void pop() {
+        int val = stackOne.pop();
+        if (stackTwo.peek() == val) {
+            stackTwo.pop();
+        }
+    }
+    
+    public int top() {
+        return stackOne.peek();
+    }
+    
+    public int getMin() {
+        return stackTwo.peek();
+    }
+}
