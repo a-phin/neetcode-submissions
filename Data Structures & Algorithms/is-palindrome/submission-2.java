1ce5717733c065b8785a9daa92a7473b99455668
Angelina Phin <vkpfxfhz48@privaterelay.appleid.com>
2026-08-24 17:00:15 -0400

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        int pointOne = 0;
+        int pointTwo = s.length() - 1;
+        while (pointOne < pointTwo) {
+            if (!Character.isLetterOrDigit(s.charAt(pointOne))) {
+                pointOne++;
+            }
+            if (!Character.isLetterOrDigit(s.charAt(pointTwo))) {
+                pointTwo--;
+            }
+            if (s.charAt(pointOne) != s.charAt(pointTwo)) {
+                return false;
+            }
+            pointOne++;
+            pointTwo--;
+        }
+        return true;
+    }
+}
