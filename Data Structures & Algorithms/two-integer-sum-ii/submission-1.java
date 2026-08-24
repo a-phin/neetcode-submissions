832e771c21ec07cd0a084f9aedcf4f9714eca4a2
Angelina Phin <vkpfxfhz48@privaterelay.appleid.com>
2026-08-24 17:16:12 -0400

    Update: two-integer-sum-ii - submission-1

--- Data Structures & Algorithms/two-integer-sum-ii/submission-1.java
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.java
@@ -1,17 +1,17 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int[] indices = new int[2];
-        int p1 = 0; // Pointer 1
-        int p2 = 1; // Pointer 2
-        while (numbers[p1] + numbers[p2] != target && p1 < numbers.length) {
-            if (p2 == numbers.length - 1 && p1 < numbers.length - 1) {
-                p2 = p1 + 1;
+        int[] sumIndices = new int[2];
+        int pointerOne = 0;
+        int pointerTwo = numbers.length - 1;
+        while (pointerOne < pointerTwo && numbers[pointerOne] + numbers[pointerTwo] != target) {
+            if (numbers[pointerOne] + numbers[pointerTwo] > target) {
+                pointerTwo--;
+            } else {
+                pointerOne++;
             }
-            p1++;
-            p2++;
         }
-        indices[0] = p1 + 1;
-        indices[1] = p2 + 1;
-        return indices;
+        sumIndices[0] = pointerOne + 1;
+        sumIndices[1] = pointerTwo + 1;
+        return sumIndices;
     }
 }
